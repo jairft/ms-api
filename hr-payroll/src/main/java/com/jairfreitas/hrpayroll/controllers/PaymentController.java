41fe241d52280e7cd9b85d8758107c884abfc7f4
jair-freitas <jair.freitas@ibm.com>
2023-03-23 14:11:40 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/jairfreitas/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/jairfreitas/hrpayroll/controllers/PaymentController.java
@@ -2,6 +2,7 @@ package com.jairfreitas.hrpayroll.controllers;
 
 import com.jairfreitas.hrpayroll.entities.Payment;
 import com.jairfreitas.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,16 @@ public class PaymentController {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = service.getPayment(workerId, days);
         return ResponseEntity.ok().body(payment);
     }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        Payment payment = new Payment("Jair", 550.0, days);
+        return ResponseEntity.ok().body(payment);
+    }
+
 }
