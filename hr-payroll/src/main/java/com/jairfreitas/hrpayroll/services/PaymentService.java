946b52d9655766ee98188a72efb9b6ad41274d6e
jair-freitas <jair.freitas@ibm.com>
2023-03-23 12:58:42 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/jairfreitas/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/jairfreitas/hrpayroll/services/PaymentService.java
@@ -4,12 +4,9 @@ import com.jairfreitas.hrpayroll.entities.Payment;
 import com.jairfreitas.hrpayroll.entities.Worker;
 import com.jairfreitas.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Service
 public class PaymentService {
