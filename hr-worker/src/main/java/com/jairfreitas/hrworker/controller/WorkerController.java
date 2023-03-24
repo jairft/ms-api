ea940e7f055f8a43c04616a5c823443c3d5805a4
jair-freitas <jair.freitas@ibm.com>
2023-03-24 08:39:50 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/jairfreitas/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/jairfreitas/hrworker/controller/WorkerController.java
@@ -30,6 +30,11 @@ public class WorkerController {
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+        try {
+            Thread.sleep(3000L);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
         logger.info("PORT = " + environment.getProperty("local.server.port"));
         return ResponseEntity.ok().body(service.findById(id));
     }
