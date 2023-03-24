7c7067c887a57ee9747f29ca081d7928e28162a0
jair-freitas <jair.freitas@ibm.com>
2023-03-24 13:28:02 -0300

    project hr-user created

---
+++ hr-user/src/test/java/com/jairfreitas/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jairfreitas.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
